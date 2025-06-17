b26fc161673723eda8aca1f0486e554ca9af1bc5
Yasir Arfat | Full-Stack Developer <hina.nida@gmail.com>
2025-06-17 09:38:37 -0700

    Locators added

--- tests/Locators_builtin.spec.js
+++ tests/Locators_builtin.spec.js
@@ -10,6 +10,17 @@ These are the recommended built-in locators.
     page.getByTitle() to locate an element by its title attribute.
     page.getByTestId() to locate an element based on its data-testid attribute (other attributes can be configured).
 */
+/*
+These are the recommended built-in locators.
+
+    page.getByRole() to locate by explicit and implicit accessibility attributes.
+    page.getByText() to locate by text content.
+    page.getByLabel() to locate a form control by associated label's text.
+    page.getByPlaceholder() to locate an input by placeholder.
+    page.getByAltText() to locate an element, usually image, by its text alternative.
+    page.getByTitle() to locate an element by its title attribute.
+    page.getByTestId() to locate an element based on its data-testid attribute (other attributes can be configured).
+*/
 test('Builtin Locators',async ({page})=>{
 
     await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
@@ -32,13 +43,5 @@ test('Builtin Locators',async ({page})=>{
     //getByText locator
 
     await expect(await page.getByText(text)).toBeVisible();
-
-
-
-
-
-
-
-
 
 })
